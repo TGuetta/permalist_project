405248a8affe6552733cbeef84a54e39c2c707e8
Tomer Gueta <tgtelhai@gmail.com>
2025-01-01 12:11:19 +0200

    connecting to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -32,3 +33,8 @@ app.post("/delete", (req, res) => {});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+// steps to solve the exercise:
+// 1. create a new database in pgaadmin called permalist with a table called items
+// 2. insert to the items table some test data (id, title) (you can see it in line 10)
+// 3. we want to be able to read the data from the database and display it in the browser. we do it by using the get method and the query method in line 18.
